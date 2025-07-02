fae6798290cec23f5a3fc5a6208ebf33fc838e80
Roms10000 <romain.richardon@outlook.fr>
2025-07-02 08:23:12 +0200

    header/footer + style

---
+++ templates/footer.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Comité monestié</title>
+    <link rel="stylesheet" href="style.css" />
+    <a href="//img.icons8.com/plumpy/24/circled-menu--v1.png>"></a>
+  </head>
+  <body>
+    <header>
+        <section class="head">
+            <article class="titre">
+                <h1 class="titre1">Comité Monestiés</h1>
+                <h2 class="titre2">Calendrier de réservation de la salle des fêtes</h2>
+            <article>
+        </section>
+        <nav class="menue">
+            <img width="24" height="24" src="https://img.icons8.com/plumpy/24/circled-menu--v1.png" alt="circled-menu--v1"/>Menue
+        </nav>
+        <article class="connect">
+          <span>Se connecter |</span>
+          <span>S'inscrire</span>
+        </article>
+    </header>
