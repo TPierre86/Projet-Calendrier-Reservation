622e1efc46864f7412dae054c6d8b83911f09378
Thibault Pierre <145769312+TPierre86@users.noreply.github.com>
2025-07-02 09:03:57 +0200

    changement dans l'arborécence

---
+++ models/gestionnaire.php
@@ -0,0 +1,20 @@
+<!-- menu burger gestionnaire -->
+<link rel="" href="styles/components/menu.css">
+<section id="burger-menu">
+    <nav id="nav-menu-open">
+        <button id="btn-close-menu"><i class="fa-solid fa-xmark"></i></button>
+        <ul>
+            <li><a href="">Gestion des Associations</a></li>
+            <li><a href="">Gestion des membres des Associations</a></li>
+            <li><a href="">Gestion des Utilisateurs</a></li>
+            <li><a href="">Gestion des Salles</a></li>
+        </ul>
+    </nav>
+    <article href="" id="nav-menu-close">
+        <button id="btn-menu"><i class="fa-solid fa-bars"></i></button>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </article>
+</section>
