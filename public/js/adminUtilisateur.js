0f2fae1b241e39566c8404e4e816aa482aeeeaf9
Natsumi26 <marion.redon26@yahoo.fr>
2025-07-03 11:36:06 +0200

    gestion utilisateur creer new utilisateur

--- public/js/adminUtilisateur.js
+++ public/js/adminUtilisateur.js
@@ -1,4 +1,17 @@
-document.getElementById('supprimer').addEventListener('click', function(){
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const boutonNouveau = document.getElementById('nouveau');
+    const formulaire = document.getElementById('form-nouveau');
 
-})
+    if (boutonNouveau && formulaire) {
+        boutonNouveau.addEventListener('click', () => {
+            // Toggle l'affichage du formulaire
+            if (formulaire.style.display === 'none') {
+                formulaire.style.display = 'block';
+                boutonNouveau.textContent = "Annuler";
+            } else {
+                formulaire.style.display = 'none';
+                boutonNouveau.textContent = "Nouveau Utilisateur";
+            }
+        });
+    }
+});
