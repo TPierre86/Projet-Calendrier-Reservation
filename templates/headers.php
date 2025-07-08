0f1be44045857a18b89ff6d8e753625ba2d72c02
Antony Houille <houille-antony@hotmail.fr>
2025-07-08 17:27:49 +0200

    content

--- templates/headers.php
+++ templates/headers.php
@@ -9,10 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Comité monestié</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/cerulean/bootstrap.min.css" />
     <link rel="stylesheet" href="/Projet-Calendrier-Reservation/public/styles/components/calendar.css" />
     <link rel="stylesheet" href="/Projet-Calendrier-Reservation/public/styles/layout/headers&footers.css" />
     <link rel="stylesheet" href="/Projet-Calendrier-Reservation/public/styles/components/menuBurger.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/cerulean/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/main.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="/Projet-Calendrier-Reservation/public/styles/components/inscription.css">
