3c981fb7367ffd3663ec85dac7e94d025444534a
Roms10000 <romain.richardon@outlook.fr>
2025-07-10 10:03:42 +0200

    rajout css pour refresh

--- models/refresh.php
+++ models/refresh.php
@@ -6,14 +6,21 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Comité monestiés</title>
-    <meta http-equiv="refresh" content="5;url=connexion.php" />
-    <link rel="stylesheet" href="refresh.css" />
+    <link rel="stylesheet" href="/Projet-Calendrier-Reservation/public/styles/components/refresh.css"">
   </head>
   <body id="bodyRefresh">
     <article>
       <p id="good">✅ Inscription réussie !</p>
-      Redirection en cours vers la page de connexion.......
+      <p>Redirection en cours vers la page de connexion<span class="progress"></span></p>
     </article>
-    <p id="bug">Si la redirection automatique ne fonctionne pas :&nbsp;<a href=http://localhost/Projet-Calendrier-Reservation/models/connexion.php>connexion</p>
+    <p id="bug">
+      Si la redirection automatique ne fonctionne pas :
+      <a href="http://localhost/Projet-Calendrier-Reservation/models/connexion.php">connexion</a>
+    </p>
+    <script>
+      setTimeout(() => {
+        window.location.href = "connexion.php";
+      }, 2000); // 2 secondes
+    </script>
   </body>
-</html
+</html>
