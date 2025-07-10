10bde1c1d5c0226801ce225a2e29df554188822e
Thibault Pierre <145769312+TPierre86@users.noreply.github.com>
2025-07-10 13:15:46 +0200

    merge

--- database/loadEvents.php
+++ database/loadEvents.php
@@ -10,9 +10,9 @@ try {
 
     $sql = "
         SELECT r.id_reservation, r.salle_id, r.date_debut, r.date_fin, r.heure_debut, r.heure_fin,
-            r.association_id, r.recurrent, r.menageCheckbox, r.menage,
+            r.association_id AS reservation_association_id, r.recurrent, r.menageCheckbox, r.menage,
             s.nom_salle,
-            u.association_id
+            u.association_id AS utilisateur_association_id
         FROM reservations r
         JOIN salles s ON r.salle_id = s.id_salle
         JOIN utilisateurs u ON r.utilisateur_id = u.id_utilisateur
@@ -20,8 +20,6 @@ try {
     ";
 
     $stmt = $pdo->query($sql);
-
-// Récupération des réservations (remplace 'id' par 'id_reservation')
 
     $events = [];
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -35,7 +33,8 @@ try {
             'start' => $start,
             'end' => $end,
             'allDay' => false,
-            'association_id' => (int)$row['association_id'],
+            // Utilise l'association de la réservation pour la couleur
+            'association_id' => (int)$row['reservation_association_id'],
             'extendedProps' => [
                 'id_reservation' => $row['id_reservation'],
                 'date_debut' => $row['date_debut'],
@@ -44,7 +43,8 @@ try {
                 'heure_fin' => $row['heure_fin'],
                 'attachments' => [],
                 'salle_id' => $row['salle_id'],
-                'association_id' => $row['association_id'],
+                // Utilise l'association de la réservation pour les droits/couleur
+                'association_id' => $row['reservation_association_id'],
                 'recurrence' => isset($row['recurrent']) ? (bool)$row['recurrent'] : false,
                 'menageCheckbox' => isset($row['menageCheckbox']) ? (bool)$row['menageCheckbox'] : false,
                 'menage' => isset($row['menage']) ? (bool)$row['menage'] : false,
