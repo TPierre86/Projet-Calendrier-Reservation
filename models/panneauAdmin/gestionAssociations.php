5dd276f1793f09b564355b55090a1aa6bc9651bd
Antony Houille <houille-antony@hotmail.fr>
2025-07-11 08:04:23 +0200

    Bouton retour

--- models/panneauAdmin/gestionAssociations.php
+++ models/panneauAdmin/gestionAssociations.php
@@ -131,17 +131,16 @@ require_once('../../templates/headers.php');
                 </table>
                 <section class="pagination">
                     <?php if ($page > 1): ?>
-                        <a href="../../controllers.php" class="btn-retour" style="font-weight:bold;text-decoration:underline;">Retour</a>
-                        <a href="?page=<?= $page - 1 ?>">&laquo; Précédent</a>
                         
+                        <a href="?page=<?= $page - 1 ?>">&laquo; Précédent</a>
                     <?php endif; ?>
                     <?php for ($i = 1; $i <= $totalPages; $i++): ?>
                         <a href="?page=<?= $i ?>" <?= $i === $page ? 'style="font-weight:bold;text-decoration:underline;"' : '' ?>><?= $i ?></a>
                     <?php endfor; ?>
                     <?php if ($page < $totalPages): ?>
                         <a href="?page=<?= $page + 1 ?>">Suivant &raquo;</a>
-                        <a href="../../controllers.php" class="btn-retour" style="font-weight:bold;text-decoration:underline;">Retour</a>
                     <?php endif; ?>
+                    <a href="../../controllers.php" class="btn-retour" style="font-weight:bold;text-decoration:underline;">Retour</a>
                     </section>
             </section>
             <!-- Formulaire HTML de modification -->
